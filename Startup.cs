90dbab9f4876340b995fd3c5b3a63359fece983c
jonathan augusto dos santos <j.santos@alexandrem.com.br>
2018-06-28 10:26:22 -0300

    Adicionado documentação swagger

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace testes_webapi
 {
@@ -17,12 +18,15 @@ namespace testes_webapi
         {
             services.AddMvc();
             services.AddDbContext<PessoasContext>((options) => options.UseInMemoryDatabase("Pessoas"));
+            services.AddSwaggerGen(s => s.SwaggerDoc("v1", new Info() { Title = "Api de consulta de pessoas", Version = "v1" }));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "Api de consulta de pessoas v1"));
 
             app.UseMvc();
         }
